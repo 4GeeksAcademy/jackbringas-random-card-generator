91d8e38f51355c04d767abf27ea017c545b768bd
Jackbringas <102544709+Jackbringas@users.noreply.github.com>
2025-02-21 19:09:02 +0000

    proyecto final

--- src/app.js
+++ src/app.js
@@ -5,7 +5,41 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+const suits = ["♦", "♥", "♠", "♣"];
+
+const valuesCard = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "A", "K", "Q", "J"];
+
+function generateRandomCard() {
+
+  const randomSuitIndex = Math.floor(Math.random() * suits.length);
+  const randomSuitValue = suits[randomSuitIndex];  
+
+  
+  const valueIndexCard = Math.floor(Math.random() * valuesCard.length);
+  const randomValueCard = valuesCard[valueIndexCard];
+
+  
+  console.log("randomCard: ${randomValueCard} ${randomSuitValue}");
+
+
+  let topSuitElement= document.getElementById("top-suit")
+  topSuitElement.innerText = randomSuitValue
+
+  
+  let cardValueElement=document.getElementById("card-value")
+  cardValueElement.innerText = randomValueCard; 
+
+  let bottomSuitElement=document.getElementById("bottom-suit")
+  bottomSuitElement.innerText = randomSuitValue;
+
+  if (randomSuitValue === "♥" || randomSuitValue === "♦") {
+    topSuitElement.classList.add("red-suit");
+    bottomSuitElement.classList.add("red-suit");
+  } else {
+    topSuitElement.classList.remove("red-suit");
+    bottomSuitElement.classList.remove("red-suit");
+  }
+}
+
+window.onload = generateRandomCard;
